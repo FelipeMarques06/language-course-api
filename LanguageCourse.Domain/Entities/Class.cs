55e672d8a37c9adb184c9a28b3239d901c205228
Luiz Felipe Marques <felipemph2@gmail.com>
2023-11-17 23:20:45 -0300

    created Domain Layer and first entities

---
+++ LanguageCourse.Domain/Entities/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageCourse.Domain.Entities
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public int AcademicYear { get; set; }
+        public DateTime Created_at { get; set; }
+    }
+}
---
+++ LanguageCourse.Domain/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageCourse.Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public DateTime Created_at { get; set; }
+    }
+}
