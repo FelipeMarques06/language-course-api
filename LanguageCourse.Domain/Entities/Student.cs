89e6a516572a780351c2a5b05a7b9bdf39c78f20
Luiz Felipe Marques <felipemph2@gmail.com>
2023-11-18 12:12:26 -0300

    Added application and infrastructure layers

--- LanguageCourse.Domain/Entities/Student.cs
+++ LanguageCourse.Domain/Entities/Student.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LanguageCourse.Domain.Entities
 {
@@ -12,6 +13,15 @@ namespace LanguageCourse.Domain.Entities
         public string Name { get; set; }
         public string Cpf { get; set; }
         public string Email { get; set; }
-        public DateTime Created_at { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Student(string name, string cpf, string email)
+        {
+            // Validation logic (cpf and email) to add here
+            Name = name;
+            Cpf = cpf;
+            Email = email;
+            CreatedAt = DateTime.UtcNow;
+        }
     }
 }
